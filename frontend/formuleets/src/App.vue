5df57ca1b37897cbe006bff09b669a534e01364a
yoannchb-pro <charbonn.yoann@gmail.com>
2022-01-11 11:38:21 -0500

    document title

--- frontend/formuleets/src/App.vue
+++ frontend/formuleets/src/App.vue
@@ -16,9 +16,51 @@ export default {
     Footer,
     GoToTop,
   },
+  data(){
+    return {
+      // All of those data are for the title of the page
+      lastTitle: "",
+      titleAnimatedInterval: null,
+
+      speed: 1000,
+
+      car: null,
+      latestChar: "="
+    }
+  },
   mounted(){
     const lg = navigator.language.split('-')[0];
     this.$root.$i18n.setLocale(lg);
+
+    document.addEventListener('visibilitychange', this.animateTitle);
+  },
+  beforeUnmount(){
+    document.removeEventListener('visibilitychange', this.animateTitle);
+  },
+  methods: {
+    animateTitle(){
+      if(document.hidden) {
+        this.lastTitle = document.title;
+
+        this.animatedCar();
+        this.titleAnimatedInterval = setInterval(this.animatedCar.bind(this), this.speed);
+      } else {
+        clearInterval(this.titleAnimatedInterval);
+        document.title = this.lastTitle;
+      }
+    },
+
+    animatedCar(){
+      if(!this.car) this.car = "==========c".split('');
+
+      const back = JSON.parse(JSON.stringify(this.latestChar));
+
+      this.latestChar = this.car.shift();
+
+      this.car.push(back);
+
+      document.title = this.car.join('').replace('c', '🚗');
+    }
   }
 }
 </script>
--- frontend/formuleets/src/router/index.js
+++ frontend/formuleets/src/router/index.js
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import meta from './meta'
+
 import Cars from '../views/3DCars/3DCars.vue'
 import Contact from '../views/Contact/Contact.vue'
 import Donate from '../views/Donate/Donate.vue'
@@ -10,60 +13,92 @@ import Sponsors from '../views/Sponsors/Sponsors.vue'
 import Team from '../views/Team/Team.vue'
 import Videos from '../views/Videos/Videos.vue'
 
+
+
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: "FormuleETS | Home"
+    }
   },
   {
     path: "/team",
     name: "Team",
-    component: Team
+    component: Team,
+    meta: {
+      title: "FormuleETS | Team"
+    }
   },
   {
     path: "/sponsors",
     name: "Sponsors",
-    component: Sponsors
+    component: Sponsors,
+    meta: {
+      title: "FormuleETS | Sponsors"
+    }
   },
   {
     path: "/project",
     name: "Project",
-    component: Project
+    component: Project,
+    meta: {
+      title: "FormuleETS | Project"
+    }
   },
   {
     path: "/3d-model",
     name: "3DModel",
-    component: Cars
+    component: Cars,
+    meta: {
+      title: "FormuleETS | 3D Model"
+    }
   },
   {
     path: "/donate",
     name: "Donate",
-    component: Donate
+    component: Donate,
+    meta: {
+      title: "FormuleETS | Donate"
+    }
   },
   {
     path: "/contact-us",
     name: "Contact",
-    component: Contact
+    component: Contact,
+    meta: {
+      title: "FormuleETS | Contact US"
+    }
   },
   {
     path: "/media/videos",
     name: "Videos",
-    component: Videos
+    component: Videos,
+    meta: {
+      title: "FormuleETS | Videos"
+    }
   },
 
   //EASTER EGG ;)
   {
     path: "/easter-eggs",
     name: "EASTEREGG",
-    component: EASTEREGG
+    component: EASTEREGG,
+    meta: {
+      title: "FormuleETS | EASTER EGGS"
+    }
   },
 
   //For 404 error, always need to be at the end
   {
     path: "/:pathMatch(.*)*",
     name: "Error404",
-    component: Error404
+    component: Error404,
+    meta: {
+      title: "FormuleETS | Not found"
+    }
   }
 ]
 
@@ -74,8 +109,10 @@ const router = createRouter({
 });
 
 //go back to top when you change page
-router.beforeEach(() => {
-  window.scrollTo(0, 0)
+router.beforeEach((to, from, next) => {
+  window.scrollTo(0, 0);
+
+  meta(to, from, next)
 })
 
 export default router
