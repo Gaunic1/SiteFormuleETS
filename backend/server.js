fbaaeb835d2b8495bd5a052e7899b99b647ff4c1
yoannchb-pro <charbonn.yoann@gmail.com>
2022-02-22 19:05:55 -0500

    backend album

---
+++ backend/server.js
@@ -0,0 +1,73 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true });
+const GetGoogleDrive = require('drive-album');
+const fs = require('fs');
+
+function TimeBetweenTwoDate(startDate, stopDate){
+    const diff = (stopDate.getTime() - startDate.getTime()) / 1000;
+    console.log(Math.abs(diff))
+    return Math.abs(diff);
+}
+
+//cors
+fastify.register(require('fastify-cors'), (instance) => (req, callback) => {
+    const origin = req.headers.origin || "localhost";
+
+    console.log("ORIGIN: " + origin);
+
+    // do not include CORS headers for requests from localhost
+    if (/localhost/.test(origin) || /formuleets/.test(origin)) {
+        callback(null, true);
+        return
+    }
+
+    callback(new Error("Not allowed"));
+});
+
+fastify.route({
+    method: 'GET',
+    url: '/album/:id',
+    handler: async (request) => {
+        const id = request.params.id;
+        const file = "./db/" + id + ".json"
+      
+        const startDate = new Date();
+      
+        try {
+            let content = {type: "scrap", content: [], date: new Date()};
+      
+            const fileExist = fs.existsSync(file);
+      
+            if(fileExist){
+              content = JSON.parse(fs.readFileSync(file, "utf8"));
+            }
+      
+            if(!fileExist || TimeBetweenTwoDate(startDate, new Date(content.date)) >= 60 * 60){
+              content.type = "scrap";
+              content.content = await GetGoogleDrive(id);
+              fs.writeFileSync(file, JSON.stringify({
+                  type: "loaded from file",
+                  id: id,
+                  content: content.content,
+                  date: new Date()
+              }));
+            }
+      
+            return content;
+        } catch(e){
+            console.error(e);
+            return {error: true, msg: e};
+        }
+      }
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
