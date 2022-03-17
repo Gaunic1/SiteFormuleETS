8f1e40734a30b7683c84489e5c61f9334430c7af
yoannchb-pro <charbonn.yoann@gmail.com>
2022-03-17 19:10:52 -0400

    camera depth

--- frontend/formuleets/src/views/3DCars/3DCars.vue
+++ frontend/formuleets/src/views/3DCars/3DCars.vue
@@ -40,7 +40,7 @@ export default {
       if(sceneExist) sceneExist.remove();
 
       const scene = new THREE.Scene();
-      const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+      const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
 
       const renderer = new THREE.WebGLRenderer({ alpha: true });
       renderer.setSize( document.body.clientWidth , window.innerHeight );
