4094029e82cdd1a20c26b67f55020d8e60b45ccd
yoannchb-pro <charbonn.yoann@gmail.com>
2022-04-14 18:06:12 -0400

    circle and bar size

--- frontend/formuleets/src/components/Date/Date.vue
+++ frontend/formuleets/src/components/Date/Date.vue
@@ -7,11 +7,11 @@
     </h2>
   </section>
 
-  <section class="h-screen w-full dark:bg-dark-mode bg-white
+  <section class="w-full dark:bg-dark-mode bg-white
   flex justify-center items-center flex-col bg-cover bg-center bg-no-repeat relative py-5" 
   lazy-background="/static/home/fond2.svg">
     <!-- BAR -->
-    <div class="bg-red-600 w-2 h-full rounded-md absolute" data-aos="zoom-in"></div>
+    <div class="bg-red-600 w-1 h-full rounded-md absolute" data-aos="zoom-in"></div>
 
     <!-- CONTENT -->
     <div v-for="(item, index) of dateList" 
@@ -21,7 +21,8 @@
 
         <div class="w-1/2 flex items-center px-8 my-10" :class="index%2 == 0 ? 'justify-end' : 'ml-auto'">
             <Countdown :date="item.date" :hour="item.hour" :title="item.title"></Countdown>
-            <div class="rounded-full bg-red-800 w-8 h-8 ml-10 absolute right-1/2 transform translate-x-1/2"></div>
+            <div class="rounded-full bg-red-800 border border-red-600 w-4 h-4
+             ml-10 absolute right-1/2 transform translate-x-1/2"></div>
         </div>
 
     </div>
