f69db3c635519c2e5310210fd7d5f4419a8cd5cc
yoannchb-pro <charbonn.yoann@gmail.com>
2022-04-17 13:31:15 -0400

    date animation test

--- frontend/formuleets/src/components/Countdown/Countdown.vue
+++ frontend/formuleets/src/components/Countdown/Countdown.vue
@@ -1,14 +1,41 @@
 <template>
-  <div class="rounded-md border dark:border-[#ffffff41] dark:bg-dark-mode
+    <div class="rounded-md border dark:border-[#ffffff41] dark:bg-dark-mode
    bg-white p-2 dark:text-white z-40 text-center">
-      <h3 class="text-sm font-bold mb-3">{{ $t(title) }}</h3>
-      <p class="text-xs">{{ $t('message.date.time', time) }}</p>
-  </div>
+        <h3 class="text-sm font-bold mb-3">{{ $t(title) }}</h3>
+        <div class="flex flex-col text-xs">
+            <div class="flex mb-3">
+                <p>Days </p>
+                <div class="flex-1 flex justify-end ml-3">
+                    <NumberDisplay :time="time[0]"></NumberDisplay>
+                </div>
+            </div>
+            <div class="flex mb-3">
+                <p>Hours </p>
+                <div class="flex-1 flex justify-end ml-3">
+                    <NumberDisplay :time="time[1]"></NumberDisplay>
+                </div>
+            </div>
+            <div class="flex mb-3">
+                <p>Minutes </p>
+                <div class="flex-1 flex justify-end ml-3">
+                    <NumberDisplay :time="time[2]"></NumberDisplay>
+                </div>
+            </div>
+            <div class="flex mb-3">
+                <p>Seconds </p>
+                <div class="flex-1 flex justify-end ml-3">
+                    <NumberDisplay :time="time[3]"></NumberDisplay>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import NumberDisplay from "./NumberDisplay.vue";
 export default {
     name: "Countdown",
+    components: { NumberDisplay },
     props: {
         title: {
             required: true,
@@ -23,51 +50,54 @@ export default {
             type: String
         },
     },
-    data(){
+    data() {
         return {
             time: [],
             stopDate: null,
             stop: true
-        }
+        };
     },
-    mounted(){
+    mounted() {
         this.stopDate = new Date(this.date + " " + this.hour);
         this.changeHour();
-
         this.interval = setInterval(this.changeHour.bind(this), 1000);
     },
-    beforeUnmount(){
+    beforeUnmount() {
         clearInterval(this.interval);
     },
     methods: {
-        calcDate(){
+        calcDate() {
             var dateEntered = this.stopDate;
             var now = new Date();
             var difference = dateEntered.getTime() - now.getTime();
-
             if (difference <= 0) {
                 // Timer done
                 clearInterval(this.time);
                 this.stop = true;
-                return [0,0,0,0];
-            } else {
+                return [0, 0, 0, 0];
+            }
+            else {
                 this.stop = false;
                 let seconds = Math.floor(difference / 1000);
                 let minutes = Math.floor(seconds / 60);
                 let hours = Math.floor(minutes / 60);
                 let days = Math.floor(hours / 24);
-
                 hours %= 24;
                 minutes %= 60;
                 seconds %= 60;
 
+                days = days.toString().padStart(2, '0').split('');
+                hours = hours.toString().padStart(2, '0').split('');
+                minutes = minutes.toString().padStart(2, '0').split('');
+                seconds = seconds.toString().padStart(2, '0').split('');
+
                 return [days, hours, minutes, seconds];
             }
         },
-        changeHour(){
+        changeHour() {
             const time = this.calcDate();
             this.time = time;
         }
-    }
+    },
 }
 </script>
---
+++ frontend/formuleets/src/components/Countdown/NumberDisplay.vue
@@ -0,0 +1,158 @@
+<template>
+    <div class="timer">
+        <div class="animated" ref="timer">
+            <div class="front">{{ time ? time[0] : 0 }}</div>
+            <div class="back">{{ backVal ? backVal[0] : 0 }}</div>
+        </div>
+        <hr class="border-bt" />
+        <div class="next">
+            <p>{{ time ? time[0] : 0 }}</p>
+        </div>
+        <p>{{ backVal ? backVal[0] : 0 }}</p>
+    </div>
+    <div class="timer">
+        <div class="animated" ref="timer2">
+            <div class="front">{{ time ? time[1] : 0 }}</div>
+            <div class="back">{{ backVal ? backVal[1] : 0 }}</div>
+        </div>
+        <hr class="border-bt" />
+        <div class="next">
+            <p>{{ time ? time[1] : 0 }}</p>
+        </div>
+        <p>{{ backVal ? backVal[1] : 0 }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NumberDisplay",
+    props: {
+        time: {
+            type: Array,
+            required: true,
+            default: () => [0,0]
+        },
+    },
+    watch: {
+        time(next){
+            if(this.backVal[0] != this.time[0]){
+                this.$refs.timer.classList.add('play-animation');
+                this.$refs.timer.addEventListener('animationend', () => {
+                    this.backVal = next;
+                    this.$refs.timer.classList.remove('play-animation');
+                }, {once: true});
+            }
+
+            if(this.backVal[1] != this.time[1]){
+                this.$refs.timer2.classList.add('play-animation');
+                this.$refs.timer2.addEventListener('animationend', () => {
+                    this.backVal = next;
+                    this.$refs.timer2.classList.remove('play-animation');
+                }, {once: true});
+            }
+        },
+    },
+    data(){
+        return {
+            backVal: null,
+        }
+    },
+    mounted(){
+        this.backVal = this.time;
+    }
+}
+</script>
+
+<style scoped>
+*{
+  box-sizing: border-box;
+  color: #fff;
+  padding: 0;
+  margin: 0;
+} 
+
+.timer div, .timer p{
+  font-weight: bold;
+}
+
+.timer{
+  position: relative;
+  width: 1rem;
+  height: 1.3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #dddddd;
+  transform-style: preserve-3d;
+  perspective: 300px;
+  background-color: #1b1b1b;
+}
+
+.next{
+  background-color: #1b1b1b;
+  position: absolute;
+  width: 100%;
+  height: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  overflow: hidden;
+  top: 0;
+}
+
+.next p{
+  transform: translateY(50%);
+}
+
+.animated{
+  position: absolute;
+  top: -1px;
+  z-index: 10;
+  width: calc(100% + 2px);
+  height: calc(50% + 1px);
+/*   background-color: #1b1b1b; */
+  display: flex;
+  justify-content: center;
+  overflow: visible;
+  transform-style: preserve-3d;
+  transform-origin: bottom;
+  border: 1px solid #dddddd;
+}
+
+.play-animation{
+      animation: 0.75s rotate linear;
+}
+
+.front, .back{
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 200%;
+  transform: rotateY(0deg) translateY(1px); /* fix firefox bug */
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  background-color: #1b1b1b;
+}
+
+.front{
+  transform-origin: center;
+  transform: translateY(1px) rotateY(180deg) rotateZ(180deg);
+}
+
+.border-bt{
+  position: absolute;
+  height: 1px;
+  width: 100%;
+  background-color: #dddddd;
+  opacity: 0.5;
+  z-index: 15;
+}
+
+@keyframes rotate{
+  to{
+    transform: rotateX(-180deg);
+  }
+}
+</style>
