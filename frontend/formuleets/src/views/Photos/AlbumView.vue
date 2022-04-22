ed705b18cc485af8c07fe73083c3d2b15ecc87b0
yoannchb-pro <charbonn.yoann@gmail.com>
2022-04-22 16:03:08 -0400

    albums interface

--- frontend/formuleets/src/views/Photos/AlbumView.vue
+++ frontend/formuleets/src/views/Photos/AlbumView.vue
@@ -9,7 +9,7 @@
     <div class="w-full text-center relative mb-10">
         <div class="absolute top-12 left-5 flex dark:text-white justify-center animate-pulse
             items-center p-3 text-2xl cursor-pointer z-10" @click="modal = true">
-                <p class="mr-3">{{ $t('message.albums.title') }}</p>
+                <p class="mr-3">{{ $t('message.albums.title_aside') }}</p>
                 <i class="fa-solid fa-circle-arrow-right"></i>
         </div>
 
@@ -30,14 +30,21 @@
         <template v-else v-for="item of db" :key="item.name">
 
             <router-link :to="'/photos/' + item.id">
-                <div class="dark:text-white
-                calc-w rounded-md dark:bg-dark-mode bg-white border dark:border-gray-100 cursor-pointer mb-5"
+                <div class="calc-w rounded-md dark:bg-dark-mode 
+                bg-white border dark:border-gray-100 cursor-pointer 
+                mb-5 h-60 overflow-hidden relative"
                 lazy-animation="zoomin" :key="item.thumbnail">
-                    <div class="flex p-2 items-center relative w-full">
-                        <i class="fas fa-image"></i>
-                        <h2 class="absolute right-2">{{ item.name }}</h2>
+
+                    <!-- TEXT -->
+                    <div class="flex p-5 justify-center items-center 
+                    absolute top-0 left-0 w-full h-full bg-overlay z-10
+                    text-center group">
+                        <h2 class="text-2xl text-white uppercase font-bold transition-all
+                        group-hover:tracking-wider">{{ item.name }}</h2>
                     </div>
-                    <div class="bg-cover bg-center w-full h-52 lazy-skeleton"
+
+                    <!-- IMAGE -->
+                    <div class="bg-cover bg-center w-full h-full lazy-skeleton"
                     :lazy-background="item.thumbnail">
                     </div>
                 </div>
