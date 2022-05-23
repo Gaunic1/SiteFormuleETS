6dfb5d445ae8f542e1582c6f01fb900413a28418
yoannchb-pro <charbonn.yoann@gmail.com>
2022-05-23 10:29:48 -0400

    link date for unveilling

--- frontend/formuleets/src/components/Date/date.js
+++ frontend/formuleets/src/components/Date/date.js
@@ -11,6 +11,7 @@ const dateList = {
             title: "message.date.title.recruitment",
             date: "06/04/2022", // mm/dd/yyyy
             hour: "21:00:00", // hours:minutes:secondes
+            link: "https://docs.google.com/forms/d/e/1FAIpQLSc7bhjknT7J-2aFIoG6hNO3s9yeVapbBpYiCrKO4NlNnNwMdg/viewform"
         },
         {
             title: "message.date.title.shootout",
