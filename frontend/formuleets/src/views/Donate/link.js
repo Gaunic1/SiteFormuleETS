4d3ed1f7a4e14ecbd491042271065cf80ae05e73
Youllou <62644404+Youllou@users.noreply.github.com>
2022-11-16 17:32:33 -0500

    New new link for donate

--- frontend/formuleets/src/views/Donate/link.js
+++ frontend/formuleets/src/views/Donate/link.js
@@ -1,8 +1,8 @@
 const link = {
   link1:
-    "https://www.jedonneenligne.org/fdets/campagne/ets/challenges/view/0680a783-39da-480c-995a-cda1707b5109",
+    "https://www.jedonneenligne.org/fdets/campagne/ets/challenges/view/e1ccbbf8-65ec-11ed-8e7f-001dd8b75df7",
   link2:
-    "https://www.jedonneenligne.org/fdets/campagne/ets/challenges/view/0680a783-39da-480c-995a-cda1707b5109"
+    "https://www.jedonneenligne.org/fdets/campagne/ets/challenges/view/e1ccbbf8-65ec-11ed-8e7f-001dd8b75df7"
 };
 
 export default link;
