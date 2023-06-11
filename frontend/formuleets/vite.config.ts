392dec2ddc17f43a04943f5960d5842c437447db
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2023-06-11 18:28:33 -0400

    removed useless depencies

--- frontend/formuleets/vite.config.ts
+++ frontend/formuleets/vite.config.ts
@@ -1,26 +1,21 @@
 import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import PrerendererWebpackPlugin from "@prerenderer/webpack-plugin";
 import routes from "./routes.match";
 import prerender from "@prerenderer/rollup-plugin";
+
+const isProd = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
 
-    //Prerendering
-    // process.env.NODE_ENV === "production" &&
-    //   (new PrerendererWebpackPlugin({
-    //     routes,
-    //   }) as any),
-
     // Generate SiteMap for Google indexation
     // process.env.NODE_ENV === "production" &&
     //   new SitemapPlugin({ base: "https://formule-ets.ca", paths: routes }),
     prerender({ routes }),
-  ].filter(Boolean),
+  ],
 
   resolve: {
     alias: {
